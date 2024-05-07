0dacf569ef2969998a1cd911c8ca19f93a1aec57
mr.root <1mr.rooter3@gmail.com>
2024-05-07 15:31:03 +0330

    add a new fich

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,13 @@
 import { XCircleIcon } from "@heroicons/react/24/outline";
 
 function Modal({ title, children, onOpen, open }) {
-  if (!open) return null;
-
   return (
     <div>
-      <div className="backdrop" onClick={() => onOpen(false)}></div>
-      <div className="modal">
+      <div
+        className={`backdrop ${open ? "show" : "hidden"}`}
+        onClick={() => onOpen(false)}
+      ></div>
+      <div className={`modal ${open ? "show" : "hidden"}`}>
         <div className="modal__header">
           <h2 className="title">{title}</h2>
           <button onClick={() => onOpen(false)}>
